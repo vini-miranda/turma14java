c04b8c0b45f62e134cbcc30ff7d3bdcff08d067e
vini-miranda <v-alves-miranda@live.com>
2020-12-07 12:16:34 -0300

    exe 4

--- java/Lista de exercícios-JAVA/Lista 1/src/Exercício_04.java
+++ java/Lista de exercícios-JAVA/Lista 1/src/Exercício_04.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 
 public class Exercício_04 {
-
+	static Scanner leia = new Scanner(System.in);
+	public static void main(String[] args) {
+		int a, b, c;
+		System.out.println("insira o primeiro número: ");
+		a = leia.nextInt();
+		System.out.println("insira o segundo número: ");
+		b = leia.nextInt();
+		System.out.println("insira o terceiro número: ");
+		c = leia.nextInt();
+		
+		int r = (a+b)*2;
+		int s = (b+c)*2;
+		int d = (r+s)/2;
+		
+		System.out.println("O resultado é " + d);
+	}
 }
